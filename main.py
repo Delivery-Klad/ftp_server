534f7b6cd246532b1a01f05b60a09d32f93a3210
Danila <dakfadeev@gmail.com>
2021-07-22 22:00:53 +0300

    get files by owner

--- main.py
+++ main.py
@@ -21,8 +21,6 @@ def db_connect():
 
 def create_tables():
     connect, cursor = db_connect()
-    cursor.execute('DROP TABLE files')
-    connect.commit()
     cursor.execute('CREATE TABLE IF NOT EXISTS files('
                    'id INTEGER PRIMARY KEY,'
                    'name TEXT NOT NULL,'
@@ -32,21 +30,17 @@ def create_tables():
     connect.close()
 
 
-create_tables()
-
-
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
     global url
     connect, cursor = db_connect()
     try:
-        name = f"{datetime.utcnow().strftime('%d-%m-%Y %H:%M:%S:%f')[:-3]}{file.filename}"
+        name = f"{datetime.utcnow().strftime('%d-%m-%Y_%H:%M:%S.%f')[:-3]}{file.filename}"
         with open(f"{name}", "wb") as out_file:
-            content = await file.read()
-            out_file.write(content)
+            out_file.write(await file.read())
         # print(os.stat(file.filename).st_size)
         cursor.execute("SELECT count(id) FROM files")
-        max_id = int(cursor.fetchall()[0][0]) + 1
+        max_id = int(cursor.fetchone()[0]) + 1
         cursor.execute(f"INSERT INTO files (id, name) VALUES ({max_id}, '{name}')")
         connect.commit()
         return f"{url}/get/file_{max_id}"
@@ -56,16 +50,29 @@ async def upload_file(file: UploadFile = File(...)):
 
 
 @app.get("/get/file_{id}")
-async def get_file(id):
+async def get_file(id: int):
     connect, cursor = db_connect()
+    cursor.execute(f"SELECT name FROM files WHERE id={id}")
     try:
-        cursor.execute(f"SELECT name FROM files WHERE id={id}")
-        try:
-            res = cursor.fetchall()[0][0]
-            print(res)
-        except IndexError:
-            return JSONResponse(status_code=404)
-        return FileResponse(f"{res}")
+        return FileResponse(f"{cursor.fetchone()[0]}")
+    except IndexError:
+        return JSONResponse(status_code=404)
     finally:
         cursor.close()
         connect.close()
+
+
+@app.get("/get/owner_files")
+async def get_owner_files(owner: str):
+    connect, cursor = db_connect()
+    cursor.execute(f"SELECT name FROM files WHERE owner='{owner}'")
+    try:
+        return FileResponse(f"{cursor.fetchall()[0][0]}")
+    except IndexError:
+        return JSONResponse(status_code=404)
+    finally:
+        cursor.close()
+        connect.close()
+
+
+create_tables()
